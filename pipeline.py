74ff8df682351be5382f674c0cec25015340ea21
mark rosetta <marked@gmail.com>
2019-12-06 06:39:03 -0600

    Bump version number in tandem

--- pipeline.py
+++ pipeline.py
@@ -64,7 +64,7 @@ if not PYTHON:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20191206.02'
+VERSION = '20191206.03'
 USER_AGENT = 'ArchiveTeam'
 TRACKER_ID = 'yahoo-groups-api'
 # TRACKER_HOST = 'tracker.archiveteam.org'  #prod-env
--- yahoo.py
+++ yahoo.py
@@ -36,7 +36,7 @@ else:
 # WARC metadata params
 
 WARC_META_PARAMS = OrderedDict([('software', 'yahoo-group-archiver'),
-                                ('version','20191206.02'),
+                                ('version','20191206.03'),
                                 ('format', 'WARC File Format 1.0'),
                                 ('command-arguments', ' '.join(sys.argv))
                                 ])
