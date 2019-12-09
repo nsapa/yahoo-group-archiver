6b16c12d48152f8d342f28e38a6a642e0b99948e
lennier1 <57744274+lennier1@users.noreply.github.com>
2019-12-09 17:40:17 -0500

    Bump version

--- yahoo.py
+++ yahoo.py
@@ -36,7 +36,7 @@ else:
 # WARC metadata params
 
 WARC_META_PARAMS = OrderedDict([('software', 'yahoo-group-archiver'),
-                                ('version','20191208.05'),
+                                ('version','20191209.01'),
                                 ('format', 'WARC File Format 1.0'),
                                 ('command-arguments', ' '.join(sys.argv))
                                 ])
