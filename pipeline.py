290034826c1b440dfaf1027f0cc72ef5c7a39812
marked <marked@users.noreply.github.com>
2019-12-13 08:38:34 -0600

    Reduce rsync concurrency
    from 20 -> 2

--- pipeline.py
+++ pipeline.py
@@ -322,7 +322,7 @@ pipeline = Pipeline(
         },
         id_function=stats_id_function,
     ),
-    LimitConcurrent(NumberConfigValue(min=1, max=20, default='20',
+    LimitConcurrent(NumberConfigValue(min=1, max=20, default='2',
                                       name='shared:rsync_threads', title='Rsync threads',
                                      description='The maximum number of concurrent uploads.'),
                     UploadWithTracker('http://%s/%s' % (TRACKER_HOST, TRACKER_ID),
